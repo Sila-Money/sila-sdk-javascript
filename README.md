7d4b0f029e3428410efc83239698785a5d8063f5
Isaac Avery <avery@silamoney.com>
2019-02-11 07:49:00 -0800

    Added code coverage to the toolset

---
+++ README.md
@@ -0,0 +1,25 @@
+# Sila SDK for Node Apps
+
+## Development
+
+### Setup
+
+> npm install
+
+That's it. You're ready to go!
+
+### Testing
+
+Run unit tests and code coverage summary
+
+> npm run test
+
+To see a detail of code coverage, view your lcov report:
+
+> http-server ./coverage/lcov-report/
+
+Then browse to the address displayed in your terminal.
+
+NOTE: If you do not have http-server installed, you can install it with the following command:
+
+> npm i -g http-server
--- src/index.js
+++ src/index.js
@@ -1,7 +1,23 @@
-function helloWorld(name) {
-  return `Hi, ${name}`;
+// regex that checks if value contains digits only
+const digitsOnly = /^\d+$/;
+
+// function to mask digits into US phone format
+function maskUSPhone(phone) {
+  // returning null when getting null, undefined, or an object that is no string as a parameter
+  if (!phone || typeof phone !== 'string') {
+    return null;
+  }
+
+  // returning the untouched value when it contains non-digit chars or when it has a length != 10
+  if (digitsOnly.test(phone) === false || phone.length !== 10) {
+    return phone;
+  }
+
+  // returning the masked value
+  const codeArea = phone.substring(0, 3);
+  const prefix = phone.substring(3, 6);
+  const sufix = phone.substring(6, 10);
+  return `(${codeArea}) ${prefix}-${sufix}`;
 }
 
-const message = helloWorld('Isaac');
-
-console.log(message);
+export default maskUSPhone;
